53ba883044adf0c506c73909a736c1de5866b918
James Jiang <james.jiang13@gmail.com>
2021-12-03 00:07:07 -0800

    Add day 2 pt 2

--- day2/day2.js
+++ day2/day2.js
@@ -24,4 +24,32 @@ function positioning(instructions) {
     console.log(res)
 }
 
-positioning(realData);
+
+
+function manualPositioning(instructions) {
+    let depth = 0, horiz = 0, aim = 0;
+    let instrucArr = instructions.split('\n');
+
+    instrucArr.forEach((command) => {
+        let parts = command.split(' ');
+        let magnitude = parseInt(parts[1])
+        switch (parts[0]){
+            case 'forward':
+                horiz += magnitude
+                depth += aim * magnitude
+                break
+            case 'down':
+                aim += magnitude
+                break
+            case 'up':
+                aim -= magnitude
+                break
+        }
+    })
+    
+    let res = depth * horiz
+    console.log(res)
+}
+
+// positioning(realData);
+manualPositioning(realData);
